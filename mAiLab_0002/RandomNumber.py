1cd33d194dab29c5f77eddd2b0250d4a3709793c
echonesis <echonesis@gmail.com>
2017-05-05 21:19:25 +0800

    mAiLab0002 Advanced #1 updated.

--- mAiLab_0002/RandomNumber.py
+++ mAiLab_0002/RandomNumber.py
@@ -15,15 +15,25 @@ if __name__ == '__main__':
 
     # Basic #2
     import numpy as np
+    import time
     print 'Basic 2>'
     n1 = [10**1, 10**2, 10**3, 10**4, 10**5]
+    usedTime = list()
     for iCnt in n1:
+        t1 = time.time()
         result1 = simpleGenRandomNumber(iCnt, -1, 1)
+        usedTime.append(time.time() - t1)
         print 'Case for N =', iCnt
         print 'Mean =', np.mean(result1), '; STD =', np.std(result1)
 
-    # For Basic part, the following is the sample output:
+    # Advanced #1
+    print 'Advanced 1>'
+    for i in range(len(n1)):
+        print 'Case for N =', n1[i]
+        print 'Used Sys Time =', usedTime[i], '(s)'
+
     '''
+    For Basic part, the following is the sample output:
     Basic 1> Generate 5 Random Number
     [0.38554052795997207, 0.6136228008082, 0.6787578153203891, 0.6489740564293955, 0.7710917185546308]
     Basic 2>
